2ad8865878bd79a9319df3dbe71c0c26b19fb0fb
WilseErik <erik.wilstermann@hotmail.com>
2023-12-18 13:02:32 +0100

    Add solution for day 17 part two

--- day_17/part_two.py
+++ day_17/part_two.py
@@ -10,7 +10,7 @@ import advent_util
 #pylint: enable=wrong-import-position
 
 CURRENT_DAY = 17
-CORRECT_EXAMPLE_ANSWER = 71
+CORRECT_EXAMPLE_ANSWER = 94
 
 
 class Node:
@@ -25,6 +25,9 @@ class Node:
 
     def __lt__(self, other):
         return self.value < other.value
+
+    def get_dir_str(self):
+        return {0:'U', 1:'R', 2:'D', 3:'L'}[self.direction]
 
     def __repr__(self):
         direction = {0:'U', 1:'R', 2:'D', 3:'L'}
@@ -56,26 +59,38 @@ def solve(input_lines):
     for r in range(n_rows):
         for c in range(n_cols):
             for d in range(4):
-                min_dist = 1
-                dist_range = 3
-                if d != UP and d != DOWN:
+                min_dist = 4
+                dist_range = 7
+                if (d != UP and d != DOWN) or (r==0 and c == 0):
                     cost = 0
+                    for i in range(min_dist-1):
+                        if (r-1-i) >= 0:
+                            cost += node_grid[UP][r-1-i][c].cost
                     for i in range(dist_range):
                         if (r-min_dist-i) >= 0:
                             cost += node_grid[UP][r-min_dist-i][c].cost
                             node_grid[d][r][c].edges.append((node_grid[UP][r-min_dist-i][c], cost))
                     cost = 0
+                    for i in range(min_dist-1):
+                        if (r+1+i) < n_rows:
+                            cost += node_grid[DOWN][r+1+i][c].cost
                     for i in range(dist_range):
                         if (r+min_dist+i) < n_rows:
                             cost += node_grid[DOWN][r+min_dist+i][c].cost
                             node_grid[d][r][c].edges.append((node_grid[DOWN][r+min_dist+i][c], cost))
-                if d != RIGHT and d != LEFT:
+                if (d != RIGHT and d != LEFT) or (r==0 and c == 0):
                     cost = 0
+                    for i in range(min_dist-1):
+                        if (c+1+i) < n_cols:
+                            cost += node_grid[RIGHT][r][c+1+i].cost
                     for i in range(dist_range):
                         if (c+min_dist+i) < n_cols:
                             cost += node_grid[RIGHT][r][c+min_dist+i].cost
                             node_grid[d][r][c].edges.append((node_grid[RIGHT][r][c+min_dist+i], cost))
                     cost = 0
+                    for i in range(min_dist-1):
+                        if (c-1-i) >= 0:
+                            cost += node_grid[LEFT][r][c-1-i].cost
                     for i in range(dist_range):
                         if (c-min_dist-i) >= 0:
                             cost += node_grid[LEFT][r][c-min_dist-i].cost
@@ -101,12 +116,17 @@ def solve(input_lines):
                        edge.prev = n
                        heapq.heappush(unchecked_nodes, (v + cost, edge))
     paths_found = [x.value!=2**15 for x in end_nodes]
+    #
+    # Print
+    #
     print([n.value for n in end_nodes])
     n = end_nodes[2]
     path = set()
+    path_nodes = []
     while n.prev != None:
         #print(n)
         path.add((n.row, n.col))
+        path_nodes.append(n)
         n = n.prev
     colorama.init()
     for r, row in enumerate(grid):
@@ -114,10 +134,9 @@ def solve(input_lines):
             if (r,c) not in path:
                 print(colorama.Fore.WHITE + str(v), end='')
             else:
-                print(colorama.Fore.GREEN + '+', end='')
+                nodes = [n for n in path_nodes if n.row==r and n.col == c]
+                print(colorama.Fore.GREEN + nodes[0].get_dir_str(), end='')
         print('')
-        #str_row = ''.join([str(v) if ((r,c) not in path) else '^' for c, v in enumerate(row)])
-        #print(str_row)
     print(colorama.Style.RESET_ALL)
     return min([n.value for n in end_nodes])
 
